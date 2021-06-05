b0d2d6d93ae37a341a91860d69f47cd663677a93
yohaim0917 <84915768+yohaim0917@users.noreply.github.com>
2021-06-05 13:46:37 +0900

    Add files via upload

---
+++ Beyond The Night/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movingspeed = 5.0f;
+    public float rotatespeed = 100f;
+    float ry;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ry = transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(0, 0, y) * movingspeed * Time.deltaTime);
+
+        ry += x * rotatespeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(new Vector3(0, ry, 0));
+    }
+}
